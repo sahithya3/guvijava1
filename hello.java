a161f61655fa0e545d576c5be69c26f87f79b18f
sahithya3 <sahithya.varshini3@gmail.com>
2017-07-10 00:11:38 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,13 @@
+// HELLO 
+package sahithya;
+import java.util.*; 
+public class hello 
+{
+	public static void main(String[]args)
+	{
+		for (int i=0; i <= 5; i++) 
+		{
+			System.out.println("Hello");
+		}
+	}
+}
