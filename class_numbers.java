e5a7a0c06ffe0088a19dc6f8ad0f577a565a4745
sahithya3 <sahithya.varshini3@gmail.com>
2017-07-10 00:20:17 +0530

    Create class_numbers.java

---
+++ class_numbers.java
@@ -0,0 +1,22 @@
+public class numbers 
+{
+	public static void main(String[]args)
+	{
+		int i;
+		Scanner s= new Scanner(System.in);
+		System.out.println("Enter any number: ");
+		i= s.nextInt();
+		if(i==0)
+		{
+			System.out.println("Number is Zero");
+		}
+		else if (i>0)
+		{System.out.println("Number is positive");
+		
+		}
+		else
+		{
+			System.out.println("Number is Odd");
+		}
+	}
+}
